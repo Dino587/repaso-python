02ea2b88a3f368d9c295197f1017ee66914ae941
Dino <al.dinosalord@iesramonllull.net>
2023-10-27 11:52:54 +0100

    mi tercer commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Hola mundo")
+print("Hola mundo")
+
+#soy duende de loco
+print("Hola mundo")
+
+"""
+Texto que no se va a interpretar, va serios
+Texto que no se va a interpretar, va serios
+Texto que no se va a interpretar, va serios
+"""
+
+#Variables
+
+texto = "Repaso de Python"
+nombre = "DINO"
+altura = "192cm"
+year = 2023
+
+print(f"{texto} - {nombre} - {altura} - {str(year)}")
+print(f"{texto} - {nombre} - {altura} - {str(year)}")
