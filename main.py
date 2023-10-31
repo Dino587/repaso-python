25499ebca98eaf1159cf1ba2bd71acf7c4ea858a
Dino587 <114218899+Dino587@users.noreply.github.com>
2023-10-31 11:56:21 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -18,4 +18,42 @@ altura = "192cm"
 year = 2023
 
 print(f"{texto} - {nombre} - {altura} - {str(year)}")
-print(f"{texto} - {nombre} - {altura} - {str(year)}")
+print(texto + " - " + nombre + " - " + altura + " - " + str(year))
+
+# Entrada
+#sitioweb = input("¿Cuál es tu página web?: ")
+#print("el sitio web del usuario es: " + sitioweb)
+
+# Condiciones
+"""
+altura = int(input("¿Cuál es tu altura?: "))
+if altura >= 180:
+    print("Eres una persona alta!!")
+else:
+    print("Eres bajito")
+"""
+
+# Funciones
+"""
+var_altura = int(input("¿Cuál es tu altura?: "))
+
+def mostrarAltura(altura):
+    resultado = " "
+
+    if altura >= 180:
+        resultado = "Eres una persona alta!!"
+    else:
+        resultado = "Eres bajito"
+
+    return resultado
+
+print(mostrarAltura(var_altura))
+"""
+
+# Listas
+personas = ["Víctor", "Paco", "Pepe"]
+
+print(personas[2])
+
+for persona in personas:
+    print("-" + persona)
